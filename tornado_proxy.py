05e93e2e18fc5a53a84474d7ab23125166f07c3d
James Brown <jbrown@yelp.com>
2012-05-15 16:09:59 -0700

    remove an unnecessary layer of wrapping

--- tornado_proxy.py
+++ tornado_proxy.py
@@ -2,9 +2,8 @@
 Wrapper for tornado.httpserver.HTTPServer which understands the HAProxy
 PROXY protocol for passing remote address information. For more information
 about this protocol, see
-    http://haproxy.1wt.eu/download/1.5/doc/proxy-protocol.txt
 
-This version only works with Tornado 1.x
+    http://haproxy.1wt.eu/download/1.5/doc/proxy-protocol.txt
 """
 
 try:
@@ -14,6 +13,7 @@ except ImportError, e:
     ssl = None
 
 import errno
+import functools
 import logging
 import socket
 
@@ -21,21 +21,21 @@ import tornado
 from tornado import iostream
 from tornado.httpserver import HTTPServer, HTTPConnection
 
-def get_proxy(stream, after):
+def _get_proxy(content, after, io_loop):
     """Read PROXY information from the given IOStream, then call the given
     callback function.
-    
+
     The callback will be passed a single argument, a tuple of the address as
     would've been returned from accept() (that is to say, ('a.b.c.d',
     80))"""
-    def read_first_line(content, after):
-        fields = content.split(" ")
-        assert fields[0] == "PROXY", "Invalid PROXY line"
-        assert fields[1] == "TCP4", "Only IPv4 is currently supported"
-        source_address = fields[2]
-        source_port = fields[4]
-        after((source_address, source_port))
-    stream.read_until("\r\n", functools.partial(read_first_line, after=after)
+    content = content.rstrip("\r\n")
+    fields = content.split(" ")
+    assert fields[0] == "PROXY", "Invalid PROXY line"
+    assert fields[1] == "TCP4", "Only IPv4 is currently supported"
+    source_address = fields[2]
+    source_port = int(fields[4])
+    # bounce this through the IOLoop for dumb reasons
+    after((source_address, source_port))
 
 class _ProxyWrappedHTTPServerTornadoOne(HTTPServer):
     def __init__(self, *args, **kwargs):
@@ -51,11 +51,12 @@ class _ProxyWrappedHTTPServerTornadoOne(HTTPServer):
                 if e[0] in (errno.EWOULDBLOCK, errno.EAGAIN):
                     return
                 raise
-            assert self.ssl_options is not None, "SSL Not supported in wrapped servers"
+            assert self.ssl_options is None, "SSL Not supported in wrapped servers"
             try:
                 stream = iostream.IOStream(connection, io_loop=self.io_loop)
-                get_proxy(stream, lambda address: HTTPConnection(stream, address, self.request_callback,
-                         self.no_keep_alive, self.xheaders))
+                stream.read_until("\r\n", functools.partial(_get_proxy, after=lambda address: HTTPConnection(stream,
+                    address, self.request_callback, self.no_keep_alive,
+                    self.xheaders), io_loop=self.io_loop))
             except:
                 logging.error("Error in connection callback", exc_info=True)
 
@@ -66,8 +67,9 @@ class _ProxyWrappedHTTPServerTornadoTwo(HTTPServer):
         return super(_ProxyWrappedHTTPServerTornadoTwo, self).__init__(*args, **kwargs)
 
     def handle_stream(self, stream, _):
-        get_proxy(stream, lambda address: HTTPConnection(stream, address, self.request_callback,
-                self.no_keep_alive, self.xheaders))
+        stream.read_until("\r\n", functools.partial(_get_proxy, after=lambda address: HTTPConnection(stream,
+            address, self.request_callback, self.no_keep_alive,
+            self.xheaders), io_loop=self.io_loop))
 
 if tornado.version_info < (2, 0, 0):
     ProxyWrappedHTTPServer = _ProxyWrappedHTTPServerTornadoOne
